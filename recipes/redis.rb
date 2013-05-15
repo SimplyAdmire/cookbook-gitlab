bf5194cbccb07452f8a7e4ee1606082d42cf1315
Sebastiaan van Parijs <svparijs@tryweb.nl>
2013-05-15 08:44:05 +0200

    [TASK] Refactor Redis Recipe
    The redis package use moved to a separate recipe as was default, and now (re)start on use.
    
    This removes the duplicate use.

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -1,12 +1,3 @@
-apt_repository "redis" do
-  uri "http://ppa.launchpad.net/chris-lea/redis-server/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "C7917B12"
-  action :add
-end
-
 package "redis-server"
 
 service "redis-server" do
