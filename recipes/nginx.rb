43442bf3df2779d6b1a46835d99b7c825842891c
Sebastiaan van Parijs <svparijs@refactory.it>
2013-05-15 17:15:51 +0200

    [TASK] Refactor Nginx recipe
    The recipe now properly places the gitlab site configuration in sites-available.
    Then places a symlink into the sites-enabled directory.
    
    It also disables the default site configuration which does nothing.
    
    The location of the socket and the root where modified in the nginx-gitlab site configuration file.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,8 +1,22 @@
 package "nginx"
 
-template "/etc/nginx/sites-enabled/gitlab" do
+template "/etc/nginx/sites-available/gitlab" do
   source "gitlab-nginx.erb"
   owner "root"
   group "root"
   mode 0644
+end
+
+bash "enable gitlab site to /etc/nginx/sites-enabled/gitlab" do
+  user "root"
+  group "root"
+  cwd "/etc/nginx"
+  code <<-EOH
+    ln -s /etc/nginx/sites-available/gitlab /etc/nginx/sites-enabled/gitlab
+  EOH
+  not_if { FileTest.exists?("/etc/nginx/sites-available/gitlab") }
+end
+
+nginx_site 'default' do
+  action :disable
 end
