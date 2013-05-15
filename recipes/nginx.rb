a7c66c9e766964d79b903ae04ab96799f39869e8
Sebastiaan van Parijs <svparijs@tryweb.nl>
2013-05-15 09:04:53 +0200

    [TASK] Refactored Nginx configuration recipe
    The recipe contained firewall stuff we don't need (yet).
    
    The recipe now installs and configures the gitlab site.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,13 +1,5 @@
 package "nginx"
 
-# open standard http port to tcp traffic only; insert as first rule
-firewall_rule "http" do
-  port 80
-  protocol :tcp
-  position 1
-  action :allow
-end
-
 template "/etc/nginx/sites-enabled/gitlab" do
   source "gitlab-nginx.erb"
   owner "root"
