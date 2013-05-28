ae540b6a2a5a6d24b8953cd7dd86666881f6093e
Sebastiaan van Parijs <svparijs@refactory.it>
2013-05-28 08:19:48 +0200

    [TASK] Refactor Nginx Symlink code
    The symlink code now works as intended via the supported link function of Chef.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -7,16 +7,6 @@ template "/etc/nginx/sites-available/gitlab" do
   mode 0644
 end
 
-bash "enable gitlab site to /etc/nginx/sites-enabled/gitlab" do
-  user "root"
-  group "root"
-  cwd "/etc/nginx"
-  code <<-EOH
-    ln -s /etc/nginx/sites-available/gitlab /etc/nginx/sites-enabled/gitlab
-  EOH
-  not_if { FileTest.exists?("/etc/nginx/sites-available/gitlab") }
-end
-
-nginx_site 'default' do
-  action :disable
+link "/etc/nginx/sites-available/gitlab" do
+  to "/etc/nginx/sites-enabled/gitlab"
 end
